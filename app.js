1cba7365c535792a8904507164ad0a41cef592e5
haniehDelfan <dfn.hanieh80@gmail.com>
2022-09-14 13:54:40 +0430

    fixing last ones

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 "use strict";
 import { productsData } from "./products.js";
 
-<<<<<<< HEAD
 const cartBtn = document.querySelector(".cart-btn");
 const cartModal = document.querySelector(".cart");
 const backDrop = document.querySelector(".backdrop");
@@ -16,21 +15,6 @@ const clearCart = document.querySelector(".cart-item-clear");
 
 let cart = [];
 let buttonsDOM = [];
-=======
-
-// const cartBtn = document.querySelector(".cart-btn");
-// const cartModal = document.querySelector(".cart");
-// const backDrop = document.querySelector(".backdrop");
-// const closeModal = document.querySelector(".cart-item-confirm");
-
-const cartItems = document.querySelector(".cart-items");
-const cartTotal = document.querySelector(".cart-total");
-// const cartContent = document.querySelector(".cart-content");
-
-const productsDOM = document.querySelector(".products-center");
-
-let cart = [];
->>>>>>> 3fff8f3e55e8b09a45536118278a003141f5743a
 // get products
 class Products {
   getProducts() {
@@ -59,29 +43,16 @@ class UI {
     });
   }
   getAddToCartBtns() {
-<<<<<<< HEAD
     const addToCartBtn = [...document.querySelectorAll(".add-to-cart")];
     buttonsDOM = addToCartBtn;
     // console.log(addToCartBtn);         (its gives us a node list)
     //converting node list to array:
     addToCartBtn.forEach((btn) => {
-=======
-    const addToCartBtn = document.querySelectorAll(".add-to-cart");
-    // console.log(addToCartBtn);         (its gives us a node list)
-    //converting node list to array:
-    const Buttons = [...addToCartBtn];
-    // console.log(Buttons);
-    Buttons.forEach((btn) => {
->>>>>>> 3fff8f3e55e8b09a45536118278a003141f5743a
       //ایدی محصولاتی که توی سبد هستن رو نشون میده که به جای باتن سبد خرید موجود در سبد باشه
       const id = btn.dataset.id;
       // console.log(id);
       //check if is this product is in cart
-<<<<<<< HEAD
       const isInCart = cart.find((p) => p.id === parseInt(id)); //product id ke migiri bebin ba kodom yeki az in id ha yekie
-=======
-      const isInCart = cart.find((p) => p.id === id); //product id ke migiri bebin ba kodom yeki az in id ha yekie
->>>>>>> 3fff8f3e55e8b09a45536118278a003141f5743a
       if (isInCart) {
         btn.innerText = "In Cart";
         btn.disabled = true;
@@ -91,24 +62,15 @@ class UI {
         event.target.innerText = "In Cart";
         event.target.disabled = true;
         //<get products from product:>
-<<<<<<< HEAD
         const addedProduct = { ...Storage.getProduct(id), quantity: 1 };
         //add to cart :
         cart = [...cart, addedProduct];
-=======
-        const addedProduct = Storage.getProduct(id);
-        //add to cart :
-        cart = [...cart, { ...addedProduct, quantity: 1 }];
->>>>>>> 3fff8f3e55e8b09a45536118278a003141f5743a
         //save cart:
         Storage.saveCart(cart); //inja cart dare update mishe
         //update cart value
         this.setCartValue(cart);
-<<<<<<< HEAD
         //add to cart item(sabad kharid)
         this.addCartItem(addedProduct); //boro chizi ke user dade ro toye DOM set kon
-=======
->>>>>>> 3fff8f3e55e8b09a45536118278a003141f5743a
       });
     });
   }
@@ -120,7 +82,6 @@ class UI {
       tempCartItems += curr.quantity; //2+1=3
       return acc + curr.quantity * curr.price;
     }, 0);
-<<<<<<< HEAD
     cartTotal.innerText = `totalPrice : ${totalPrice} $`;
     cartItems.innerText = tempCartItems;
   }
@@ -226,11 +187,6 @@ class UI {
     button.innerText = "Add to cart";
     button.disabled = false;
   }
-=======
-    cartTotal.innerText = `totalPrice : ${totalPrice.toFixed(2)} $`;
-    cartItems.innerText = tempCartItems;
-  }
->>>>>>> 3fff8f3e55e8b09a45536118278a003141f5743a
 }
 
 //storage
@@ -240,7 +196,6 @@ class Storage {
     localStorage.setItem("products", JSON.stringify(products));
     //برو توی لوکال استوریج مرورگر و ست کن یک دیتا رو به نام پروداکتس --(ما نمیتونیم اری اف ابجکت رو توی اسنوریج ذخیره کنیم فقط استرینگ)
   }
-<<<<<<< HEAD
 
   // یه دونه پروداکت رو از بین پروداکت ها میگیره و توی لوکال استوریج
   static getProduct(id) {
@@ -291,48 +246,3 @@ backDrop.addEventListener("click", closeModalFunction);
 // nav bar
 const navSide = document.querySelector(".fa-solid fa-bars");
 navSide.addEventListener("click");
-=======
-  /// یه دونه پروداکت رو از بین پروداکت ها میگیره و توی لوکال استوریج
-  static getProduct(id) {
-    const _products = JSON.parse(localStorage.getItem("products"));
-    return _products.find((p) => {
-      p.id === parseInt(id);
-    }); //boro id ke behet dadamo add kon
-  }
-
-  static saveCart(
-    cart //inja cart bere save beshe
-  ) {
-    localStorage.setItem("cart", JSON.stringify(cart));
-  }
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  //موقعی که دام داره لود میشه اپ رو اجرا کن
-  // console.log("loaded");
-  const products = new Products(); //دیتاها رو از کلس پرودکتس میگیرم
-  const productsData = products.getProducts(); //ریترن میکنه
-  // console.log(productsData);
-  const ui = new UI();
-  ui.displayProducts(productsData); //دیتا ها رو پاس میدم به دیس پلی
-  Storage.saveProducts(productsData);
-  ui.getAddToCartBtns();
-});
-
-//cart items modal
-// function showModalFunction() {
-//   backDrop.style.display = "block";
-//   cartModal.style.opacity = "1";
-//   cartModal.style.top = "20%";
-// }
-
-// function closeModalFunction() {
-//   backDrop.style.display = "none";
-//   cartModal.style.opacity = "0";
-//   cartModal.style.top = "-100%";
-// }
-
-// cartBtn.addEventListener("click", showModalFunction);
-// closeModal.addEventListener("click", closeModalFunction);
-// backDrop.addEventListener("click", closeModalFunction);
->>>>>>> 3fff8f3e55e8b09a45536118278a003141f5743a
